ecc6dbd9d3724c648aa195a08026cab0b64db1a7
Michael Brombach <mbrombach@yahoo.com>
2023-02-23 12:51:47 -0500

    Nav divs hiding/unhiding and link modal functional

--- app.js
+++ app.js
@@ -1,10 +1,43 @@
 
+
+
+
 $(() => {
+    const $homeDivButton = $('#home')
+    const $bioDivButton = $('#bio')
+    const $resumeDivButton = $('#resume')
+    const $projectDivButton = $('#projects')
+    const $linkWindowButton = $('#links')
+    const $contentDiv = $('.contentDiv')
+    const $closeButton = $('.close')
+    
+   
+    $homeDivButton.on('click', () => {
+        $contentDiv.css('display', 'none')
+        $('#homeDiv').css('display', 'block')
+    })
 
+    $bioDivButton.on('click', () => {
+        $contentDiv.css('display', 'none')
+        $('#bioDiv').css('display', 'block')
+    })
 
+    $resumeDivButton.on('click', () => {
+        $contentDiv.css('display', 'none')
+        $('#resumeDiv').css('display', 'block')
+    })
 
-    $('button').on('click', () => {
-        console.log('clicked')
+    $projectDivButton.on('click', () => {
+        $contentDiv.css('display', 'none')
+        $('#projectDiv').css('display', 'block')
+    })
+
+    $linkWindowButton.on('click', () => {
+        $('.linkModal').css('display', 'block')
+    })
+
+    $closeButton.on('click', () => {
+        $('.linkModal').css('display', 'none')
     })
 
 })
